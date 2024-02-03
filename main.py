2b082b6972f9c6655711df368394bbecd89cd391
enaples <enaples@users.noreply.github.com>
2024-02-03 20:34:20 +0100

    removed logging config and model updated

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from pipeline.prompts import PROMPTS
 import os
 import logging
 
-# Logging
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s [%(levelname)s] %(message)s")
-
 # Make sure the user want to run the pipeline with all prompts
 confirm = input(f"Run all prompt? (y/n): ")
 if confirm.lower() != 'y':
@@ -16,4 +10,4 @@ if confirm.lower() != 'y':
     exit()
 for pr_name, prompt in PROMPTS.items():
     logging.info(f"----------------------------------------------\nRunning {pr_name} prompt.")
-    Pipeline.pipe("./test_contracts", vul_tool='slither', model='gpt-4', output_path=os.path.join('output', pr_name), lambda_prompt=prompt)
+    Pipeline.pipe("./test_contracts", vul_tool='slither', model="gpt-4-0125-preview", output_path=os.path.join('output', "gpt-4-0125-preview", pr_name), lambda_prompt=prompt)
