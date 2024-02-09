49d897a9030474647ed486cf79352e3ba0d8e45f
enaples <enaples@users.noreply.github.com>
2024-02-09 18:10:12 +0100

    added N tries with same prompt to the pipeline

--- main.py
+++ main.py
@@ -10,10 +10,20 @@ confirm = input(f"Run all prompt? (y/n): ")
 if confirm.lower() != 'y':
     logging.info("Exiting.")
     exit()
-for pr_name, prompt in PROMPTS.items():
-    logging.info(f"----------------------------------------------\nRunning {pr_name} prompt.")
-    Pipeline.pipe("./test_contracts", vul_tool='slither', model="gpt-4-0125-preview", output_path=os.path.join('output', "gpt-4-0125-preview", pr_name), lambda_prompt=prompt)
+
+# Temperature test
+Pipeline.pipe("./test_contracts", model="gpt-4-0125-preview", output_path=os.path.join('output', "gpt-4-0125-preview", 'Preliminar', pr_name), lambda_prompt=prompt)
+
+
+# Fixed temperature 0.5, evaluate 4 shots
+n_iter = 4
+
+for i in range(1, n_iter+1):
+    for pr_name, prompt in PROMPTS.items():
+        logging.info(f"----------------------------------------------\nRunning {pr_name} prompt.")
+        Pipeline.pipe("./test_contracts", model="gpt-4-0125-preview", output_path=os.path.join('output', "gpt-4-0125-preview", str(i), pr_name), temperatures=[0.5], lambda_prompt=prompt)
+
 
 
 ### Post-processing
-SmartCMetrics.pipe(os.path.join(os.path.expanduser('~'), 'slither_shared', 'output' ))    
+SmartCMetrics.pipe(os.path.join(os.path.expanduser('~'), 'slither_shared', 'output'))    
